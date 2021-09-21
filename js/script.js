375fe0ca374f4248a6a7b2ed37c61856487112bc
game54 <ap_nbs@hotmail.com>
2021-09-22 00:15:21 +0300

    sticky-nav,fixed links

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ allLinks.forEach(function (link) {
 ///////////////////////////////////////////////////////////
 // Sticky navigation
 
-const sectionHeroEl = document.querySelector(".section-hero");
+const sectionHeroEl = document.querySelector(".hero-section");
 
 const obs = new IntersectionObserver(
   function (entries) {
